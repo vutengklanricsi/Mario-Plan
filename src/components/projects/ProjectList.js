6e1f841e80737ebf685b82d9953bda057a691a60
vutengklanricsi <richard.vuongtanquang@gmail.com>
2020-09-09 12:41:26 +0200

    5th Dashboard component made and linking

---
+++ src/components/projects/ProjectList.js
@@ -0,0 +1,31 @@
+import React from 'react';
+
+const ProjectList = () => {
+  return (
+    <div className="project-list section">
+      <div className="card z-depth-0 project-summary">
+        <div className="card-content grey-text text-darken-3">
+          <span className="card-title">Project Title</span>
+          <p>Posted by the Net Ninja</p>
+          <p className="grey-text">septemer 3 valami</p>
+        </div>
+      </div>
+      <div className="card z-depth-0 project-summary">
+        <div className="card-content grey-text text-darken-3">
+          <span className="card-title">Project Title2</span>
+          <p>Posted by the Net Ninja</p>
+          <p className="grey-text">septemer 3 valam3232i</p>
+        </div>
+      </div>
+      <div className="card z-depth-0 project-summary">
+        <div className="card-content grey-text text-darken-3">
+          <span className="card-title">Project Title3</span>
+          <p>Posted by the Net Ninja</p>
+          <p className="grey-text">septemettttr 3 valami</p>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default ProjectList;
