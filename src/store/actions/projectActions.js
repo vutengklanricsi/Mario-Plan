05c0a498d2a21f4b45798229149040808f6ed203
vutengklanricsi <richard.vuongtanquang@gmail.com>
2020-09-14 14:09:42 +0200

    18th adding project to firestore

--- src/store/actions/projectActions.js
+++ src/store/actions/projectActions.js
@@ -1,7 +1,20 @@
 export const createProject = (project) => {
-  return (dispatch, getState) => { // make async call to database
-    dispatch({ type: 'CREATE_PROJECT', project: project });
+  return (dispatch, getState, {getFirebase, getFirestore}) => { // make async call to database
+    const firestore = getFirestore(); // referál a firestore adatbázisra
+    firestore.collection('projects').add({ // ez egy asynchronus hívás.Időbe telik neki
+      ...project,
+      authorFirstName: 'Net',
+      authorLastName: 'Ninja',
+      authorId: 12345,
+      createdAt: new Date(),
+    }).then(() => {//referálunk projects nevű collection-re . hozzáadunk egy egy új dokumentumot az add funkcióval a collection-be.
+      // egy objektumot adunk át neki, ami a dokumentumra referál amit hozzáadunk a collection-be  
+      dispatch({ type: 'CREATE_PROJECT', project: project });
     // MEGKAPJUK A PROJECT-ET és hozzáadhatjuk az adatbázishoz és egy asynchron hívást csinálni(tenni)
     // és azután dispatcheljük a jelenlegi action-t
+    }).catch((err) => {
+      dispatch({ type: 'CREATE_PROJECT_ERROR', err: err });
+    })
+        
   }
 }
--- src/store/reducers/projectReducer.js
+++ src/store/reducers/projectReducer.js
@@ -9,9 +9,14 @@ const initialState = {
 const projectReducer = (state = initialState, action) => {
   switch (action.type) {
     case 'CREATE_PROJECT':
-      console.log('created project', action.project)
+      console.log('created project', action.project);
+      return state;
+    case 'CREATE_PROJECT_ERROR':
+      console.log('created project error', action.err);
+      return state;
+    default:
+      return state;
   }
-  return state  
 }
 
 export default projectReducer;
